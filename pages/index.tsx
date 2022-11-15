0160bf075c5e7a9cd661b8fc732ba0322ef30e06
Paulo Vanderley <vanderley.1109@gmail.com>
2022-11-15 15:37:00 -0300

    updating

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,12 @@
-export default function Home() {}
+export default function Home() {
+  return (
+    <Template>
+      <div className={styles.container}>
+        <Head>
+          <title>Geek Express Solutions</title>
+          <meta name="description" content="geek,express,solutions"
+        </Head>
+      </div>
+    </Template>
+  )
+}
