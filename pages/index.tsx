2145e8f82dc70a6ddc789bd8ae037e8ff28205e7
Paulo Vanderley <vanderley.1109@gmail.com>
2022-11-17 11:02:11 -0300

    adicionando template e styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
+import { Template } from "../Components/Template";
+import styles from "../styles/Home.module.css";
+
 export default function Home() {
   return (
     <Template>
-      <div></div>
+      <div className={styles.container}></div>
     </Template>
   );
 }
