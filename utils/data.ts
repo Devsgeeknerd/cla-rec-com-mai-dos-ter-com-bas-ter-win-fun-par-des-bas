db4e7a1a0be1ef31d763e0d6b636ee8f06668776
Paulo Vanderley <vanderley.1109@gmail.com>
2023-12-11 11:50:49 -0300

    removendo utils

--- utils/data.ts
+++
@@ -1,7 +0,0 @@
-export const navigationLinks = [
-  { label: "Inicio", path: ["/"] },
-  { label: "Sobre", path: ["/about"] },
-  { label: "Serviços", path: ["/services"] },
-  { label: "Projetos", path: ["/projects"] },
-  { label: "Contatos", path: ["/contacts"] },
-];
